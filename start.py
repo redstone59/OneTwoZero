8675a75866463adaabbe69f4228d1269c5aa3174
redstone59 <redstonepro23@googlemail.com>
2026-04-03 18:40:02 +1000

    Fix unfinished repeating event example

--- start.py
+++ start.py
@@ -1,6 +1,6 @@
 from python_obs.clients import OBS
 from one_two_zero.core import OneTwoZero
-from one_two_zero.decorators import otz_event_seconds
+from one_two_zero.decorators import otz_event_seconds, otz_event_repeating
 
 KILL_TIME = 70
 otz = OneTwoZero(KILL_TIME)
@@ -18,9 +18,10 @@ def ten_minutes_left(obs: OBS):
 def thirty_minutes_left(obs: OBS):
     obs.call_macro("120 30 minutes left")
 
+@otz_event_repeating(otz, 1)
 def test_repeating(obs: OBS):
     source = obs.scene("120 Desktop").source("warning tex")
-    source.
+    source.toggle_visiblity()
 
 if __name__ == "__main__":
     otz.start_stream()
